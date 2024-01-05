c7c0833e8f6bdeec740d242a54d4b998f7457ec7
misuno <alex@misuno.org>
2024-01-06 03:03:51 +0400

    New version - some cleaning

--- early-init.el
+++
@@ -1,4 +0,0 @@
-;; (add-to-list 'default-frame-alist '(font . "FiraCode 11"))
-;; (add-to-list 'default-frame-alist '(line-spacing . 0.1))
-    (add-to-list 'default-frame-alist '(font . "FiraCode 11" ))
-    (set-face-attribute 'default t :font "FiraCode 11" )
